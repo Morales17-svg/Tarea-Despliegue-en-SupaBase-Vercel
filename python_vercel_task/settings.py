0f3d2278911813bdcab51baf4acedbecf6f0864b
Morales17 <avimorales58@gmail.com>
2024-11-16 17:31:56 -0300

    decimo quinto commit

--- python_vercel_task/settings.py
+++ python_vercel_task/settings.py
@@ -12,6 +12,15 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
